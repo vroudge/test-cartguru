4aaeca620dc488195053937da17b00869fe9874a
vroudge <v.roudge@gmail.com>
2018-06-23 11:42:12 +0200

    Add defeat condition and add looping for victory-case search

--- src/index.js
+++ src/index.js
@@ -1,51 +1,72 @@
 const inputParser = require('./lib/inputParser')
 const logger = require('./lib/logger')
+const _cloneDeep = require('lodash.clonedeep')
+
+  // TODO: case of no possible victory whatsoever
 
 ;(async () => {
   try {
-    const parsedBotInput = await inputParser(`${__dirname}/../input`)
-    // freeze parsed input so we dont accidentally mutate it further down the line
-    run(Object.freeze(parsedBotInput))
+    const { towerRange, bots } = await inputParser(`${__dirname}/../input`)
+
+    // we could recurse instead of looping
+    // but readability would be severly impaired
+    // lets loop instead
+    let gameVictory = false
+    let tries = 0
+
+    while (!gameVictory) {
+      // run a deep copy of bots or we might get issues with referencing ;)
+      gameVictory = run({ towerRange: towerRange + tries, bots: _cloneDeep(bots) })
+      ++tries
+    }
 
   } catch (e) {
-    console.error(e)
+    logger('red', e)
   }
 })()
 
 const run = ({ towerRange, bots }) => {
-  logger('green', `Tower range is ${towerRange}m`)
+  logger('white', `Tower range is ${towerRange}m`)
 
-  let gameRunning = false
+  // init flag for gameloop
+  let init = false
   // current game turn
   let currentTurn = 1
-  // get bot ids so we can work properly with the dictionnary
-  let botIds = Object.keys(bots)
   // create a new botlist
-  let botList = { ...bots }
+  let botList = bots
+
+  // get bot ids so we can work properly with the dictionnary
+  let botIds = Object.keys(botList)
   let leftEnemies = undefined
 
-  while (!gameRunning || leftEnemies) {
-    gameRunning = true
-    leftEnemies = areThereAliveBots(bots, botIds)
+  while (!init || leftEnemies) {
+    //logger('white', `Current turn: ${currentTurn}`)
+    init = true
+    leftEnemies = areThereAliveBots(botList, botIds)
+
     if (!leftEnemies) {
-      logger('green', `You win in ${currentTurn-1} turns`)
-      break
+      logger('green', `You win in ${currentTurn - 1} turn${currentTurn > 1 ? 's' : ''}`)
+      return true
     }
     const { canFire, targetBotId } = towerFireOnEnemy(towerRange, botList, botIds)
 
     if (canFire) {
       const botKilled = botList[targetBotId]
       botKilled.isAlive = false
-      logger('red', `Turn ${currentTurn}: Kill ${botKilled.name} at ${botKilled.currentDistance}m`)
+      logger('magenta', `Turn ${currentTurn}: Kill ${botKilled.name} at ${botKilled.currentDistance}m`)
     }
 
     // for all alive bots move them for their speed value
-    const newBotList = moveBots(botList, botIds)
-    botList = { ...newBotList }
+    // and check if they reach the tower
+    const { updatedBotList, defeat } = moveBots(botList, botIds)
 
-    if (currentTurn > 10) {
-      break
+    if (defeat) {
+      logger('red', `You lose in ${currentTurn} turn${currentTurn > 1 ? 's' : ''}`)
+      return false
     }
+
+    botList = { ...updatedBotList }
+
     currentTurn++
   }
 
@@ -62,7 +83,8 @@ const towerFireOnEnemy = (towerRange, bots, botIds) => {
 
   for (let botId of botIds) {
     const currentBot = bots[botId]
-    const botIsInRange = currentBot.currentDistance <= towerRange
+    // check that we can fire on the bot
+    const botIsInRange = currentBot.currentDistance <= towerRange && currentBot.currentDistance !== 0
     // bot is in range, bot is alive, and we either have no target
     // or the previous selected target is further from the tower than the target we're checking
     if (botIsInRange && currentBot.isAlive && (!targetBotId || bots[targetBotId].currentDistance > currentBot.currentDistance)) {
@@ -76,15 +98,20 @@ const towerFireOnEnemy = (towerRange, bots, botIds) => {
 
 // move bots of their speed
 const moveBots = (bots, botIds) => {
+  let defeat = false
   for (let botId of botIds) {
     const currentBot = bots[botId]
 
     if (currentBot.isAlive) {
       currentBot.currentDistance -= currentBot.speed
+      currentBot.currentDistance = currentBot.currentDistance <= 0 ? 0 : currentBot.currentDistance
+      if (currentBot.currentDistance <= 0) {
+        defeat = true
+      }
     }
   }
 
-  return bots
+  return { updatedBotList: bots, defeat }
 }
 
 process.on('unhandledRejection', (data, promise) => {
